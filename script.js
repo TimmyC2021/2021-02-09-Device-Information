7c7ca2ee7b9455e1c985cb2b0683fe7593856678
Tim Carr <github@timcarr.co.uk>
2021-02-09 15:45:33 +0000

    Update 2 Window width and height added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function getData(event) {
     screenWidth = window.screen.width;
     screenHeight = window.screen.height;
+    windowWidth = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    windowHeight = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
     document.getElementById("screenWidth").innerHTML = screenWidth;
     document.getElementById("screenHeight").innerHTML = screenHeight;
+    document.getElementById("windowWidth").innerHTML = windowWidth;
+    document.getElementById("windowHeight").innerHTML = windowHeight;
 }
