878b40f2074f1ee040f8a441bb03e28e1a42a3f6
Tim Carr <github@timcarr.co.uk>
2021-02-09 15:31:28 +0000

    Update 1 screen width and height

---
+++ script.js
@@ -0,0 +1,6 @@
+function getData(event) {
+    screenWidth = window.screen.width;
+    screenHeight = window.screen.height;
+    document.getElementById("screenWidth").innerHTML = screenWidth;
+    document.getElementById("screenHeight").innerHTML = screenHeight;
+}
